d95901c1d6d6cd078b8332e0dbad5b0d121cca39
Robert Schumann <robert.schumann@unic.com>
2018-03-26 10:31:40 +0200

    add blubird promises

--- mongooseCreateType.ts
+++ mongooseCreateType.ts
@@ -1,4 +1,5 @@
 import * as _ from 'lodash'
+import * as Promise from 'bluebird'
 
 export default function createType(type, findBy, params): Promise {
     return type.findOne(findBy).lean().exec()
