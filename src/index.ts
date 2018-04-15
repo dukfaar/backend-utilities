7cd58b5ee02f90394f15fb767bbb880007e1c950
Dukfaar <dukfaar@gmail.com>
2018-04-15 15:33:26 +0200

    added index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import getProjection from './getProjection'
+import getProjectionForPath from './getProjectionForPath'
+import mongooseCreateType from './mongooseCreateType'
+import mongooseUpdateType from './mongooseUpdateType'
+import reduceSelections from './reduceSelections'
+
+export {
+    getProjection,
+    getProjectionForPath,
+    mongooseCreateType,
+    mongooseUpdateType,
+    reduceSelections   
+}
